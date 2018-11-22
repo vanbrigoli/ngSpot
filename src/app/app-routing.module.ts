2c4ab5a9e29493b887c38952a3934937239f6acd
Giovanni Brigoli <giovanni.brigoli@nokia.com>
2018-11-22 19:39:37 +0800

    fix ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { HomeComponent } from './home/home.component';
+import { ContentComponent } from './content/content.component';
 import { PaymentComponent } from './payment/payment.component';
 
 const appRoutes: Routes = [
@@ -13,11 +14,19 @@ const appRoutes: Routes = [
     path: 'home', component: HomeComponent
   },
   {
-    path: 'users', component: MainNavComponent, data: {header: 'Users'}
+    path: 'users', component: MainNavComponent, data: {header: 'Users'}, children: [
+      {
+        path: '', component: ContentComponent
+      }
+    ]
   },
   {
-    path: 'pay', component: PaymentComponent,  data: {header: 'Pay'}
-  }
+    path: 'pay', component: MainNavComponent, data: {header: 'Pay'}, children: [
+      {
+        path: '', component: PaymentComponent
+      }
+    ]
+  },
 ];
 
 @NgModule({
