423fe55afe9dded42d6f8ec5d1a32be1e8231990
Giovanni Brigoli <giovanni.brigoli@nokia.com>
2018-11-23 12:45:14 +0800

    add user functionality

--- src/app/main-nav/user-content/user-form/user-form.component.ts
+++ src/app/main-nav/user-content/user-form/user-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { User, UsersService } from '../../../services/users.service';
 
@@ -10,18 +10,27 @@ import { User, UsersService } from '../../../services/users.service';
 })
 export class UserFormComponent implements OnInit {
   users: User[] = [];
-  firstName = new FormControl('', [Validators.required]);
-  lastName = new FormControl('', [Validators.required]);
-  dateJoined = new FormControl({value: '', disabled: true}, [Validators.required]);
+  userForm;
+  firstName;
+  lastName;
+  dateJoined;
 
   constructor(private usersService: UsersService) { }
 
   ngOnInit() {
     this.users = this.usersService.users;
+    this.userForm = new FormGroup({
+      'firstName': new FormControl('', [Validators.required]),
+      'lastName': new FormControl('', [Validators.required]),
+      'dateJoined': new FormControl({value: ''}, [Validators.required])
+    });
+    this.firstName = this.userForm.get('firstName');
+    this.lastName = this.userForm.get('lastName');
+    this.dateJoined = this.userForm.get('dateJoined');
   }
 
   onAddUser() {
-    const newUser = new User(this.firstName.value, this.lastName.value, this.dateJoined.value);
+    const newUser = new User(this.firstName, this.lastName, this.dateJoined);
     this.usersService.addUser(newUser);
   }
 }
