26944d41c6bcba20eb6555ce56ccc2596e262ac9
Giovanni Brigoli <giovanni.brigoli@nokia.com>
2018-12-04 19:29:28 +0800

    refactor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyCjyWefPwZ-hGi2j5Na4jNuG2bxDnXiyNQ',
-    authDomain: 'ng2-spot.firebaseapp.com',
-    databaseURL: 'https://ng2-spot.firebaseio.com',
-    projectId: 'ng2-spot',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
     storageBucket: '',
-    messagingSenderId: '320675569928'
+    messagingSenderId: ''
   }
 };
 
